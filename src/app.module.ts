20a2750aa9b36f682b17764579297977f7dfcd79
124c4a11 <124c4a11@mail.ru>
2022-05-23 18:10:31 +0300

    DB: connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: '.env',
+        }),
+      ],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        database: configService.get('POSTGRES_DB'),
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USERNAME'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        synchronize: configService.get('DB_SYNC'),
+        entities: [__dirname + 'dist/**/*.entity.{js,ts}'],
+        autoLoadEntities: true,
+        logging: true,
+      }),
+    }),
+    UsersModule,
+  ],
+})
 export class AppModule {}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: false })
+  banned: boolean;
+
+  @Column({ nullable: true })
+  banReason: string;
+}
