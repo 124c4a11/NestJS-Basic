c2c4de484229030ba44319b54d98ea3a730060c7
124c4a11 <124c4a11@mail.ru>
2022-05-31 17:57:58 +0300

    Validation Pipe

--- src/users/dto/assign-role.dto.ts
+++ src/users/dto/assign-role.dto.ts
@@ -1,10 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNumber } from 'class-validator';
 import { UserRoles } from 'src/types/user-roles';
 
 export class AssignRoleDto {
   @ApiProperty({ example: 1, description: 'user id' })
+  @IsNumber({}, { message: 'must be a number' })
   readonly userId: number;
 
   @ApiProperty({ example: 'ADMIN', description: 'user role' })
+  @IsEnum(UserRoles, {
+    message: "must be a valid enum value: 'ADMIN' or 'USER'",
+  })
   readonly value: UserRoles;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({ example: 'John Doe', description: 'user name' })
+  @IsString({ message: 'must be a string' })
   readonly name: string;
 
   @ApiProperty({ example: 'john@email.com', description: 'user email' })
+  @IsEmail({ message: 'must match an email format' })
   readonly email: string;
 
   @ApiProperty({ example: 'userpassword1234', description: 'user password' })
+  @IsString({ message: 'must be a string' })
+  @Length(4, 16, {
+    message: 'must be not less than 4, not mutch than 16 characters',
+  })
   readonly password: string;
 }
